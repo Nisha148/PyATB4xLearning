055929d2070054e70b67a1a07f30b59ece70dd0a
nisha <nisanjinigowda@gmail.com>
2024-09-25 15:34:59 +0100

    Homework files for the 24th Aug 2024 added

--- src/Home_Work/Home_3.py
+++ src/Home_Work/Home_3.py
@@ -37,11 +37,3 @@ for i in range(1, 101):
     else:
         print(i)
 
-#Task 9
-
-num = int(input("Enter the number"))
-print(num)
-fact = 1
-for i in range(1, num + 1, 1):
-    fact = fact * i
-print(fact)
---
+++ src/Home_Work/Home_4.py
@@ -0,0 +1,8 @@
+#Task 9
+
+num = int(input("Enter the number"))
+print(num)
+fact = 1
+for i in range(1, num + 1, 1):
+    fact = fact * i
+print(fact)

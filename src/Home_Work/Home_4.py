e152e71e6ae3c241b90b7b948533af186cadf17c
nisha <nisanjinigowda@gmail.com>
2024-10-02 19:36:49 +0100

    homework files for the 29th Aug 2024 added

--- src/Home_Work/Home_4.py
+++ src/Home_Work/Home_4.py
@@ -1,4 +1,4 @@
-#Task 9
+# Task 9
 
 num = int(input("Enter the number"))
 print(num)
@@ -6,3 +6,13 @@ fact = 1
 for i in range(1, num + 1, 1):
     fact = fact * i
 print(fact)
+
+n = int(input("Enter Number: "))
+a = 0
+b = 1
+print(a, b, end=" ")
+for i in range(0, n+1):
+    c = a + b
+    print(c, end=" ")
+    a = b
+    b = c
